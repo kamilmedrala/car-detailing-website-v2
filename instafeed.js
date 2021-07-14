75980e18b17e442ac81c61d658de361c3edc5574
Kamil Mędrala <81763542+kamilmedrala@users.noreply.github.com>
2021-07-15 00:46:02 +0200

    Add files via upload

--- instafeed.js
+++ instafeed.js
@@ -1,54 +1,39 @@
+$.ajax({
+  url: "./data.json",
+  type: "GET",
+  dataType: "json",
+  success: function (response) {
+    let AccessToken = response.accessToken;
+    getPosts(AccessToken);
+  },
+});
 
+function getPosts(token) {
   $.ajax({
-    url: "./data.json",
+    url: `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${token}`,
     type: "GET",
-    dataType: "json",
+    crossDomain: true,
+    dataType: "jsonp",
     success: function (response) {
-      let AccessToken = response.accessToken;
-      getPosts(AccessToken);
+      let post = response.data;
+      for (let i = 0; i < 6; i++) {
+        let PostElement = document.createElement("div");
+        PostElement.className = "gallery__insta-image show";
+        PostElement.style.backgroundImage =
+          "url" + "(" + post[i].media_url + ")";
+        PostElement.innerHTML = "<p>" + post[i].caption + "</p>";
+        document.getElementById("id_gallery__insta").appendChild(PostElement);
+      }
     },
   });
-
-  function getPosts(token) {
-    $.ajax({
-      url: `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${token}`,
-      type: "GET",
-      crossDomain: true,
-      dataType: "jsonp",
-      success: function (response) {
-        let post = response.data;
-        for (let i = 0; i < 6; i++) {
-          let PostElement = document.createElement("div");
-          PostElement.className = "gallery__insta-image";
-          PostElement.style.backgroundImage =
-            "url" + "(" + post[i].media_url + ")";
-          PostElement.innerHTML = "<p>" + post[i].caption + "</p>";
-          document.getElementById("id_gallery__insta").appendChild(PostElement);
-        }
-      },
-    });
-  }
-
+}
+var NextToShow = document.getElementsByClassName("show");
+var delay = 0;
 
 window.onload = function () {
-  var NextToShow = document.getElementsByClassName("gallery__insta-image"); //used by images
-  var delay = 0;
-  var controller = new ScrollMagic.Controller();
-
-
-
-  //posts display animation
-  var imageScene = new ScrollMagic.Scene({
-    triggerElement: NextToShow,
-  })
-    .on("start", function fadeDelay() {
-      for (let i = 0; i < 6; i++) {
-        NextToShow[i].style.animationDelay = delay + "ms";
-        delay = delay + 50;
-      }
-      delay = 0;
-    })
-    .setClassToggle(".gallery__insta-image", "show")
-    .reverse(false)
-    .addTo(controller);
+    for (let i = 0; i < 6; i++) {
+      NextToShow[i].style.animationDelay = delay + "ms";
+      delay = delay + 50;
+    }
+    delay = 0; 
 };
